52324026fa5a71ba8518d41fedbc159b16571b0a
nusubbotin <nu.subbotin@ya.ru>
2022-09-28 14:55:35 +0300

    Задание 1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,23 @@
 public class Main {
-    public static void main(String[] args) {
+    public static int[] generateRandomArray() {
+        java.util.Random random = new java.util.Random();
+        int[] arr = new int[30];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = random.nextInt(100_000) + 100_000;
+        }
+        return arr;
+    }
 
+    public static void task1(){
+        System.out.println("Задание 1:");
+        int[] arr = generateRandomArray();
+        int summ = 0;
+        for (int i : arr) {
+            summ+= i;
+        }
+        System.out.println("Сумма трат за месяц составила " + summ + " рублей");
+    }
+    public static void main(String[] args) {
+        task1();
     }
 }
